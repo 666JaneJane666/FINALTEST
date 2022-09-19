2b0ffaccdf1b7f12d3c48ae5aea9ff396461a13f
666JaneJane666 <109788123+666JaneJane666@users.noreply.github.com>
2022-09-19 21:14:30 +0800

    Add program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] arrayOne = new string[5] {"666", "12", "mewo", "cat", "test111"};
+string[] arrayTwo = new string[arrayOne.Length];
+void Solution(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+    if(arrayOne[i].Length <= 3)
+        {
+        arrayTwo[count] = arrayOne[i];
+        count++;
+        }
+    }
+}
+void Print(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Solution(arrayOne, arrayTwo);
+Print(arrayTwo);
